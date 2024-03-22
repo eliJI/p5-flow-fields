d9dc5fa3b43baf7e9878134620d00a39634198f3
eliJI <elijahingr@gmail.com>
2024-03-22 16:57:43 -0400

    updated webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode:'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'min.js',
