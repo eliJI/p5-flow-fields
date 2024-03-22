d2b48dbe44244b997e7c9df5438c44909188d761
eliJI <elijahingr@gmail.com>
2024-03-22 16:32:50 -0400

    added files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import P5 from 'p5';
+var main = document.getElementById('main');
+var sketch = function (p5) {
+    p5.setup = function () {
+        p5.createCanvas(document.documentElement.scrollWidth, document.documentElement.clientHeight);
+    };
+    p5.draw = function () {
+        p5.background(220);
+        p5.ellipse(50, 50, 80, 80);
+    };
+};
+if (main !== null) {
+    new P5(sketch, main);
+}
