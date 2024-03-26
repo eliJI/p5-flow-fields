55eb14b678cd081addc19f99739acfc6da261a0e
eliJI <elijahingr@gmail.com>
2024-03-26 03:19:32 -0400

    fixed annoyed translation shit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
 import P5 from 'p5'
 
 const main = document.getElementById('main');
+let grid: number[][] = [];
+const RESOLUTION = 0.05//0.1;
 
 const sketch = (p5: P5) => {
     p5.setup = function() {
-        p5.createCanvas(document.documentElement.scrollWidth, document.documentElement.clientHeight);
+    
+
+        let width = document.documentElement.scrollWidth;
+        let height = document.documentElement.clientHeight;
+
+        let numCols:number = width/1 * RESOLUTION;
+        let numRows:number = height/1 * RESOLUTION;
+        p5.createCanvas(width, height);
+
+        for (let i = 0; i < numRows; i++) {
+            grid[i] = [];
+            for (let j = 0; j < numCols; j++) {
+              grid[i][j] = Math.random();
+            }
+        }
+        console.log(grid);
     }
 
     p5.draw = function () {
         p5.background(220);
-        p5.ellipse(50,50,80,80);
+    
+        let r = 0
+        let c = 0
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                p5.translate(j/RESOLUTION,i/RESOLUTION)
+                let ellipse = p5.ellipse(0, 0,5,5);
+                p5.translate(-(j/RESOLUTION),-(i/RESOLUTION))
+              //ellipse.translate(j/RESOLUTION,i/RESOLUTION)
+            
+
+            }
+        }
+       
+    
     }
 }
 
