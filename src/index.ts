72194a46db1291835691f7f5122149197398bbaf
eliJI <elijahingr@gmail.com>
2024-05-06 02:45:17 -0400

    fix: added files

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,17 @@ const main = document.getElementById('main');
 let grid: number[][] = [];
 const RESOLUTION = 0.1//0.1;
 
+
 const sketch = (p5: P5) => {
     p5.setup = function() {
+        console.log("wht")
     
 
         let width = document.documentElement.scrollWidth;
         let height = document.documentElement.clientHeight;
 
-        let numCols:number = width/2 * RESOLUTION;
-        let numRows:number = height/2 * RESOLUTION;
+        let numCols:number = width * RESOLUTION;
+        let numRows:number = height * RESOLUTION;
         p5.createCanvas(width, height);
 
         for (let i = 0; i < numRows; i++) {
@@ -31,9 +33,11 @@ const sketch = (p5: P5) => {
         let c = 0
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid[0].length; j++) {
+                p5.push()
                 p5.translate(j/RESOLUTION,i/RESOLUTION)
-                let ellipse = p5.ellipse(5, 5,5,5);
-                p5.translate(-(j/RESOLUTION),-(i/RESOLUTION))
+                p5.rotate(grid[i][j])
+                p5.line(0,-2,0,2)
+                p5.pop()
               //ellipse.translate(j/RESOLUTION,i/RESOLUTION)
             
 
@@ -42,6 +46,10 @@ const sketch = (p5: P5) => {
        
     
     }
+}
+
+function findClosestpoint(grid: number[][]) {
+    
 }
 
 if (main !== null) {
