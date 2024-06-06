3b827ef61b48a993478c68c054c90ab24f83c25e
eliJI <elijahingr@gmail.com>
2024-06-06 15:48:01 -0400

    fix: added dist folder for compiled files

--- src/index.ts
+++ src/index.ts
@@ -7,51 +7,41 @@ const RESOLUTION = 0.1//0.1;
 
 const sketch = (p5: P5) => {
     p5.setup = function() {
-        console.log("wht")
-    
-
         let width = document.documentElement.scrollWidth;
         let height = document.documentElement.clientHeight;
-
-        let numCols:number = width * RESOLUTION;
-        let numRows:number = height * RESOLUTION;
+        let numCols: number = width * RESOLUTION;
+        let numRows: number = height * RESOLUTION;
         p5.createCanvas(width, height);
-
         for (let i = 0; i < numRows; i++) {
             grid[i] = [];
             for (let j = 0; j < numCols; j++) {
-              grid[i][j] = Math.random();
+                grid[i][j] = Math.random();
             }
         }
         console.log(grid);
     }
 
-    p5.draw = function () {
+    p5.draw = function() {
         p5.background(220);
-    
-        let r = 0
-        let c = 0
+        let r = 0;
+        let c = 0;
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid[0].length; j++) {
                 p5.push()
-                p5.translate(j/RESOLUTION,i/RESOLUTION)
-                p5.rotate(grid[i][j])
-                p5.line(0,-2,0,2)
-                p5.pop()
-              //ellipse.translate(j/RESOLUTION,i/RESOLUTION)
-            
-
+                p5.translate(j / RESOLUTION, i / RESOLUTION);
+                p5.rotate(grid[i][j]);
+                p5.line(0, -2, 0, 2);
+                p5.pop();
+                //ellipse.translate(j/RESOLUTION,i/RESOLUTION)
             }
         }
-       
-    
     }
 }
 
 function findClosestpoint(grid: number[][]) {
-    
+
 }
 
 if (main !== null) {
- new P5(sketch, main)   
+    new P5(sketch, main);
 }
